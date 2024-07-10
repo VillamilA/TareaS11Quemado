424be2d3841bff6e15aaa06941a3d978dcaf1a32
VillamilA <angel.villamil@epn.edu.ec>
2024-07-10 17:13:06 -0500

    LOGIN ingresdo los datos quemados

--- src/login.java
+++ src/login.java
@@ -3,31 +3,41 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class login extends JFrame {
-    private JTextField textField1;
+    private JTextField user;
     private JPanel acceso;
-    private JPasswordField passwordField1;
+    private JPasswordField password;
     private JButton Login;
     private JLabel usuario;
     private JLabel contra;
     private JFrame frame;
 
-    public login (){
+    public login() {
         super("Acceso");
         setContentPane(acceso);
         Login.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                biografia ventana_n = new biografia();
-                ventana_n.iniciar();
-                dispose();
 
+                String contra = "POO123";
+                String usuario = "avillamil";
+                String contraIngresada =  password.getText();
+                String UsuarioIngresado = user.getText();
+                if(contra.equals(contraIngresada) && usuario.equals(UsuarioIngresado)) {
+                    biografia ventana_n = new biografia();
+                    ventana_n.iniciar();
+                    dispose();
+                }
+                else{
+                    JOptionPane.showMessageDialog(null, "Datos Incorrectos");
+                }
             }
         });
     }
-    public void iniciar(){
+
+    public void iniciar() {
         setVisible(true);
         setLocationRelativeTo(null);
-        setSize(300,400);
+        setSize(300, 400);
         // para cerrrrar
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
